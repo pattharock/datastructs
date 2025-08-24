835ec76a47e11efa03239b5045610fd231d13ac7
Ritvik Singh <ritvik1609@gmail.com>
2025-08-24 17:08:38 +0800

    added insert() for circular linked list

--- src/linkedList/ds/circularLinkedList.js
+++ src/linkedList/ds/circularLinkedList.js
@@ -29,6 +29,44 @@ export class CircularLinkedList {
     return true;
   }
 
+  insert(element, index) {
+    if (index < 0 || index > this.count) return false;
+
+    const node = new CircularNode(element);
+
+    if (this.head === null) {
+      this.head = node;
+      this.head.next = this.head;
+      this.count++;
+
+      return true;
+    }
+
+    if (index === 0) {
+      let current = this.head.next;
+
+      while (current.next !== this.head) {
+        current = current.next;
+      }
+
+      current.next = node;
+      node.next = this.head;
+      this.head = node;
+      this.count++;
+      return true;
+    }
+
+    let previousNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      previousNode = previousNode.next;
+    }
+    let nextNode = previousNode.next;
+    previousNode.next = node;
+    node.next = nextNode;
+    this.count++;
+    return true;
+  }
+
   getHead() {
     return this.head ?? undefined;
   }
--- src/linkedList/tests/ds/circularLinkedList.test.js
+++ src/linkedList/tests/ds/circularLinkedList.test.js
@@ -4,6 +4,7 @@ import { CircularLinkedList } from '../../ds/circularLinkedList.js';
 
 /**@type {CircularLinkedList} */
 let cll;
+
 describe('Circular Linked List - functional tests', () => {
   beforeEach(() => {
     cll = new CircularLinkedList();
@@ -21,6 +22,7 @@ describe('Circular Linked List - functional tests', () => {
     expect(cll.isEmpty()).toBe(true);
     expect(cll.size()).toBe(0);
     expect(cll.getHead()).toBeUndefined();
+
     cll.push(1);
     expect(cll.toString()).toBe('1');
     expect(cll.size()).toBe(1);
@@ -78,5 +80,62 @@ describe('Circular Linked List - functional tests', () => {
         element: 1,
       })
     );
+  });
+
+  it('INSERT(element, index) functions as expected', () => {
+    expect(cll.isEmpty()).toBe(true);
+    expect(cll.size()).toBe(0);
+    expect(cll.getHead()).toBeUndefined();
+
+    expect(cll.insert(200, 1)).toBe(false);
+
+    expect(cll.insert(1, 0)).toBe(true);
+    expect(cll.isEmpty()).toBe(false);
+    expect(cll.size()).toBe(1);
+    expect(cll.getHead()).toEqual(
+      expect.objectContaining({
+        element: 1,
+      })
+    );
+
+    expect(cll.insert(2, 0)).toBe(true);
+    expect(cll.isEmpty()).toBe(false);
+    expect(cll.size()).toBe(2);
+    expect(cll.getHead()).toEqual(
+      expect.objectContaining({
+        element: 2,
+      })
+    );
+    expect(cll.toString()).toEqual('2 1');
+
+    expect(cll.insert(3, 2)).toBe(true);
+    expect(cll.isEmpty()).toBe(false);
+    expect(cll.size()).toBe(3);
+    expect(cll.getHead()).toEqual(
+      expect.objectContaining({
+        element: 2,
+      })
+    );
+    expect(cll.toString()).toEqual('2 1 3');
+
+    expect(cll.insert(3, -1)).toBe(false);
+    expect(cll.isEmpty()).toBe(false);
+    expect(cll.size()).toBe(3);
+    expect(cll.getHead()).toEqual(
+      expect.objectContaining({
+        element: 2,
+      })
+    );
+    expect(cll.toString()).toEqual('2 1 3');
+
+    expect(cll.insert(3, 4)).toBe(false);
+    expect(cll.isEmpty()).toBe(false);
+    expect(cll.size()).toBe(3);
+    expect(cll.getHead()).toEqual(
+      expect.objectContaining({
+        element: 2,
+      })
+    );
+    expect(cll.toString()).toEqual('2 1 3');
   });
 });
